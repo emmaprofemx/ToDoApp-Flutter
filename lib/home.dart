b732c0b9048e8cc32308481594be89c1bd6f31ad
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-01-23 12:13:26 -0600

    Creando la barra de busqueda

--- lib/home.dart
+++ lib/home.dart
@@ -12,10 +12,43 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is home Screen.'),
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          searchBox(),
+        ],
+      ),
       ),
     );
   }
+Widget searchBox(){
+  return Container(
+            //Separando un poco del borde de izquierda a derecha
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            //Personalizando el txtField
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0), 
+                //Agregando el boton de la lupa
+                prefixIcon: Icon(
+                  Icons.search ,
+                  color: tdBlack,
+                  size: 20,),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20 , 
+                    minWidth: 25,) ,
+                    //Linea azul que aparece debajo del txtField , desactivado
+                    border: InputBorder.none , 
+                    hintText: 'Buscar...' ,
+                    hintStyle: TextStyle(color: tdGrey),
+              ),
+            ),
+          );
+}
 
 //Personalizacion del AppBar
   AppBar _buildAppBar() {
