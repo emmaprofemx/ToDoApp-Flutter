da7041173b084a9f813d3fbef94dda56a420e82a
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-01-23 11:57:42 -0600

    Construyendo el AppBar

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:todoapp/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Text('This is home Screen.'),
+      ),
+    );
+  }
+
+//Personalizacion del AppBar
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(
+          Icons.menu , 
+          color: tdBlack , 
+          size: 30,
+        ) , 
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/rayitosfondo.png'),
+          ),
+        )
+      ],) 
+      );
+  }
+}
